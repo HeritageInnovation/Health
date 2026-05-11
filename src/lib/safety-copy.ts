b4a39e0469cbd482e68114ecfe2061a6547deaad
HeritageInnovation <265192172+HeritageInnovation@users.noreply.github.com>
2026-05-11 10:56:59 +0800

    Centralize required safety disclaimer copy

---
+++ src/lib/safety-copy.ts
@@ -0,0 +1,8 @@
+export const MEDICAL_SAFETY_DISCLAIMER =
+  "這不是診斷。本應用只提供健康教育、生活方式建議、健身與營養方向、香港醫療導航及一般保險教育，不能取代醫生或持牌專業人士。";
+
+export const EMERGENCY_ESCALATION_COPY =
+  "如有胸痛、嚴重呼吸困難、中風徵象、昏迷、嚴重出血、嚴重過敏反應或其他危急情況，請立即致電 999 或前往急症室。";
+
+export const INSURANCE_SAFETY_DISCLAIMER =
+  "本內容只屬一般保險教育，並非保險、法律或財務建議，亦不保證索償結果。請向你的保險公司、持牌保險中介或相關專業人士確認。";
